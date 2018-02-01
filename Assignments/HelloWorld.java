c5539f038f95d3ffa744dc346f22386f9be21ae6
Tommy Bennett <33736953+tspeedb@users.noreply.github.com>
2018-02-01 11:21:19 -0800

    Uploaded HelloWorld.java with actual code

--- Assignments/HelloWorld.java
+++ Assignments/HelloWorld.java
@@ -1,1 +1,7 @@
-//HelloWorld
+public class HelloWorld {
+    public static void main(String[] args){
+        System.out.println("Hello World"); //Prints out the string on a new line
+    }
+}
+
+//Soft tab

88475c78d0051e85b17c4933a76ca6b646a1456a
Tommy Bennett <33736953+tspeedb@users.noreply.github.com>
2018-04-24 12:34:16 -0700

    Created DynamicChangeMaker.java

---
+++ CMSI-186_Projects/homework07/DynamicChangeMaker.java
@@ -0,0 +1,56 @@
+//Tuple - Collection of integers or Integer arrays
+//Tuple t1 = newTuple(b);
+//int[] d = new int[b.length];
+
+//[rows] [columns]
+//Tuple[][] = new Tuple [denom][cols + 1]
+
+//@param, @return, @see, @throws
+
+/** ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+ * File name  :  DynamicChangeMaker.java
+ * Purpose    :  Class that determines smallest number of coins to create a given amount with a given denomination
+ * @author    :  Tommy Bennett
+ * Date       :  2017-04-22
+ * Description:  This program provides a class that determines smallest number of coins to create a given amount with a given denomination.
+ *               This class is intended to be used as part of homework 7, the coin changemaker, which is a "Dynamic Programming" algorithm.
+ * Notes      :  None
+ * Warnings   :  None
+ *
+ *  ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+ * Revision History
+ * ================
+ *   Ver      Date     Modified by:   Reason for change or modification
+ *  -----  ----------  -------------  ---------------------------------------------------------------------
+ *  1.0.0  2017-04-19  Tommy Bennett  Initial writing
+ *
+ *  ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ */
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Arrays;
+
+public class DynamicChangeMaker {
+
+    //public static Tuple[][] = null;
+
+    /**
+     *  Constructor goes here
+     */
+
+    // cols = Integer.parseInt(args[1]) +1;
+    // rows =
+    //
+    // Tuple[][] theTable = new Tuple[rows][cols];
+
+    /**
+     *  The main program starts here
+     *  remember the constraints from the project description
+     *  @see  http://bjohnson.lmu.build/cmsi186web/homework04.html
+     *  be sure to make LOTS of tests!!
+     *  remember you are trying to BREAK your code, not just prove it works!
+     */
+    public static void main(String [] args){
+
+    }
+}
